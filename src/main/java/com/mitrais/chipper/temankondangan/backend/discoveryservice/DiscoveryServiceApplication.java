3b9625a667d1674ff6df9ce6310a29f3dcf395b4
akbarb24 <akbar.binaji@gmail.com>
2020-06-16 14:47:52 +0800

    enable admin server

--- src/main/java/com/mitrais/chipper/temankondangan/backend/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backend/discoveryservice/DiscoveryServiceApplication.java
@@ -1,11 +1,13 @@
 package com.mitrais.chipper.temankondangan.backend.discoveryservice;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
